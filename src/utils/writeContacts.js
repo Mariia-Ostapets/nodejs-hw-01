27ebf06f296bf7e31a42463c4dcd2e3b6e51f599
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-11-28 17:32:40 +0200

    Done all tasks

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -3,10 +3,8 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
   const data = JSON.stringify(updatedContacts);
-
   try {
     await fs.writeFile(PATH_DB, data, 'utf8');
-    console.log('Дані успішно записані у файл.');
   } catch (err) {
     console.error(err);
   }
